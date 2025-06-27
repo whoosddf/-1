86b8556d64dc889f92542f526e0d19b98b331643
As1_でぇ <sslsls@outlook.jp>
2025-06-27 10:59:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+function summonUnit(type) {
+  const unit = unitData[type];
+  const playerSide = document.getElementById("player-side");
+
+  const unitDiv = document.createElement("div");
+  unitDiv.className = "unit";
+  unitDiv.innerText = `${unit.name}（${unit.skill}）`;
+  playerSide.appendChild(unitDiv);
+
+  console.log(`${unit.name} が出撃！`);
+}
 
